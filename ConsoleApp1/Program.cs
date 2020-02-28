6d54c536803f4a8bc3ac724fba9ddddedfbe92aa
KirillYakub <kirillyakubchinskiy@gmail.com>
2020-02-28 10:07:59 +0300

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Example
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Variant 20 - kub raznosti chisel ");
+            Console.WriteLine("Vvedite 2 chisla: ");
+            
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c = a - b;
+
+            Console.WriteLine($"Raznica = {c}");
+
+            double d = Math.Pow(c, 3);
+            Console.WriteLine($"Kub raznosti chisel = {d}");
+
+            
+        
+        }
+    }
+}
